06c160abe8468a087ffa12fbd0a8c0c902d6acc4
Andrés Rodríguez <darkknight64@gmail.com>
2020-03-02 17:42:11 -0500

    4,5,6 session

---
+++ clase4.js
@@ -0,0 +1,10 @@
+var nombre = 'Andres', edad = 28
+
+function imprimirEdad(n,e) {
+    console.log(`${n} tiene ${e} años`);    
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky', 28)
+imprimirEdad('Eric',24)
+imprimirEdad('Darío',27)
---
+++ clase6.js
@@ -0,0 +1,19 @@
+var andres = {
+    nombre: 'Andrés',
+    apellido: 'Rodríguez',
+    edad: 28
+}
+
+var diego = {
+    nombre: 'Diego',
+    apellido: 'Guerra',
+    edad: 29
+}
+
+function imprimirNombreEnMayusculas({nombre}) {
+    console.log(nombre.toUpperCase());
+}
+
+imprimirNombreEnMayusculas(andres)
+imprimirNombreEnMayusculas(diego)
+imprimirNombreEnMayusculas({nombre :'pepito'})
